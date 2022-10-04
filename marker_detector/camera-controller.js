63c0c2e0ae4f1ce8c6ec8e6dbe05a2c8d735900e
Raul Alcantara <ralcanta@mit.edu>
2022-10-04 15:21:16 -0400

    extracted info in a CameraController class

---
+++ marker_detector/camera-controller.js
@@ -0,0 +1,103 @@
+function diff(a, b) {
+  return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+class CameraController {
+  constructor(cameraMatrix, distCoeffs, height, width, cameraConstraints) {
+    console.log("start");
+    console.log(cameraMatrix);
+    console.log(distCoeffs);
+    console.log(height);
+    console.log(width);
+    console.log(cameraConstraints);
+    console.log("yay!");
+    this.cameraMatrix = cameraMatrix;
+    this.distCoeffs = distCoeffs;
+    this.isCameraActive = false;
+    this.height = height;
+    this.width = width;
+    this.cameraConstraints = cameraConstraints;
+    this.stream = null;
+
+    this.src = new cv.Mat(height, width, cv.CV_8UC4);
+    this.dst = new cv.Mat(height, width, cv.CV_8UC1);
+  }
+  async activateCamera() {
+    this.isCameraActive = true;
+    try {
+      this.stream = await window.navigator.mediaDevices.getUserMedia(
+        this.cameraConstraints
+      );
+      console.log(this.cameraConstraints);
+      console.log(this.stream);
+      return this.stream; //TODO: use it for videoObj.srcObject = stream;
+    } catch {
+      function handle(err) {
+        console.log(err);
+      }
+    }
+  }
+  deactivateCamera() {
+    this.isCameraActive = false;
+  }
+  /**
+   * Takes the data from one Canvas and writes it to another Canvas
+   * @param {*} imageData
+   * @param {*} src Matrix
+   * @param {*} dst Matrix
+   */
+  findArucoCodes(imageData) {
+    //, cameraMatrix, distCoeffs) {
+    //   let src = new cv.Mat(height, width, cv.CV_8UC4);
+    //   let dst = new cv.Mat(height, width, cv.CV_8UC1);
+    this.src.data.set(imageData.data);
+    cv.cvtColor(this.src, this.dst, cv.COLOR_RGBA2RGB, 0);
+    let dictionary = new cv.Dictionary(cv.DICT_6X6_250);
+    let markerCorners = new cv.MatVector();
+    let markerIds = new cv.Mat();
+    cv.detectMarkers(this.dst, dictionary, markerCorners, markerIds);
+    if (markerIds.rows === 0) {
+      //Nothing detected
+      return;
+    }
+    let response = {}; //id -> {rvec: [], tvec: []}
+
+    cv.drawDetectedMarkers(this.dst, markerCorners, markerIds);
+    let rvecs = new cv.Mat();
+    let tvecs = new cv.Mat();
+    cv.estimatePoseSingleMarkers(
+      markerCorners,
+      0.1,
+      cameraMatrix,
+      distCoeffs,
+      rvecs,
+      tvecs
+    );
+    for (let i = 0; i < markerIds.rows; i++) {
+      let rvec = cv.matFromArray(3, 1, cv.CV_64F, [
+        rvecs.doublePtr(0, i)[0],
+        rvecs.doublePtr(0, i)[1],
+        rvecs.doublePtr(0, i)[2],
+      ]);
+      let tvec = cv.matFromArray(3, 1, cv.CV_64F, [
+        tvecs.doublePtr(0, i)[0],
+        tvecs.doublePtr(0, i)[1],
+        tvecs.doublePtr(0, i)[2],
+      ]);
+      cv.drawAxis(this.dst, cameraMatrix, distCoeffs, rvec, tvec, 0.1);
+      let marker_id = markerIds.data[4 * i]; //TODO: FIgure out a way this can generalize for bigger ids
+      response[marker_id] = { rvec: rvec.data64F, tvec: tvec.data64F };
+      // rvec.delete();
+      // tvec.delete();
+    }
+    // if (response[1] && response[12]) {
+    //   console.log(diff(response[1]["tvec"], response[12]["tvec"]));
+    // }
+    return response;
+  }
+}
+
+/**
+ * Green: first coordinate
+ * Blue: second coordinate
+ */
---
+++ marker_detector/constants.js
@@ -0,0 +1,60 @@
+let cameraMatrix;
+let distCoeffs;
+let cameraConstraints = {
+  audio: false,
+  video: {
+    width: 640,
+    height: 360,
+    frameRate: 30,
+    deviceId:
+      "900a49dfda6bfee6faf44cbb6b6b1cd7477dccb9c7ac304255fbac5c8d81f3d0",
+  },
+};
+const FPS = 30;
+
+if (typeof cv !== "undefined") {
+  onReadyConstants();
+} else {
+  document
+    .getElementById("opencvjs")
+    .addEventListener("load", onReadyConstants);
+}
+function onReadyConstants() {
+  /*Camera values*/
+  cameraMatrix = cv.matFromArray(
+    3,
+    3,
+    cv.CV_64F,
+    [
+      1181.1162112265451, 0.0, 948.8233981926403, 0.0, 1181.6472180982591,
+      541.2092582739663, 0.0, 0.0, 1.0,
+    ]
+  );
+  distCoeffs = cv.matFromArray(
+    5,
+    1,
+    cv.CV_64F,
+    [
+      0.19174712176744907, -0.08277619494389733, 0.0015721897019070517,
+      0.011591628661818381, -1.5351916635013496,
+    ]
+  );
+  cameraMatrix = cv.matFromArray(
+    3,
+    3,
+    cv.CV_64F,
+    [
+      9.6635571716090658e2, 0, 2.0679307818305685e2, 0, 9.6635571716090658e2,
+      2.9370020600555273e2, 0, 0, 1,
+    ]
+  );
+  distCoeffs = cv.matFromArray(
+    5,
+    1,
+    cv.CV_64F,
+    [
+      -1.5007354215536557e-3, 9.8722389825801837e-1, 1.7188452542408809e-2,
+      -2.6805958820424611e-2, -2.3313928379240205,
+    ]
+  );
+}
