a19e2b79f0baf829873c8caf75063842ee7558d3
Raul Alcantara <raulalcantara@dhcp-10-29-83-15.dyn.mit.edu>
2023-04-26 16:19:10 -0400

    added js script to handle bluetooth and camera handlers

---
+++ physical-ui-handler.js
@@ -0,0 +1,7 @@
+/**
+ * In this file there are the methods for the physical version of the Doodlebot UI.
+ *
+ * Most important methods are to:
+ *  1) Handle Bluetooth connections with the Doodlebots.
+ *  2) Handle connecting to the physical camera.
+ */
